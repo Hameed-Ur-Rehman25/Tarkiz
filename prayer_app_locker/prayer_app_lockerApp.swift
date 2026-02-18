5c77a8efa7b908890acc2b7cb3466e148652e041
Hameed <hameedkhan944873@gmail.com>
2026-02-18 16:47:26 +0500

    Initial Commit

---
+++ prayer_app_locker/prayer_app_lockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  prayer_app_lockerApp.swift
+//  prayer_app_locker
+//
+//  Created by Hameed on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct prayer_app_lockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

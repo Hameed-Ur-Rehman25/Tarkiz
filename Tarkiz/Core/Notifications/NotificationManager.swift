7c8c1ac182675c1ce33d23674b69441d3de927c2
Hameed <hameedkhan944873@gmail.com>
2026-02-26 07:14:07 +0500

    fix(notifications): import Combine to resolve ObservableObject conformance

--- Tarkiz/Core/Notifications/NotificationManager.swift
+++ Tarkiz/Core/Notifications/NotificationManager.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UserNotifications
 import SwiftUI
+import Combine
 
 /// A simple manager to handle requesting user notification permissions.
 class NotificationManager: ObservableObject {
